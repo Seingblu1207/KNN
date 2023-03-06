6013a2853746f628c310097193b2c7f982379ed0
Seingblu1207 <oveorozco@gmail.com>
2023-03-05 19:40:08 -0500

    Asserts base

--- taller2/src/taller2/Taller2.java
+++ taller2/src/taller2/Taller2.java
@@ -14,25 +14,40 @@ public class Taller2 {
         Dataset conocidos = Dataset.generateClusters2D(5);
         double[][] points = conocidos.getPointsArray();
 
-        double[][] desconocidos = new double[3][2];
+        double[][] desconocidos = new double[2][2];
 
-        // for (int i = 0; i < desconocidos.length; i++) {
-        // desconocidos[i] = (double) (RandomSeq());
-        // }
+        double[][] Datos = new double[2][2];
 
+        int[] clases = new int[2];
+
+        Datos[0][0] = 1;
+        Datos[0][1] = 1;
+
+        Datos[1][0] = 0;
+        Datos[1][1] = 0;
+
+        clases[0] = 1;
+        clases[1] = 0;
+        
         desconocidos[0][0] = 0.1;
         desconocidos[0][1] = 0.1;
 
         desconocidos[1][0] = 0.9;
         desconocidos[1][1] = 0.9;
 
-        desconocidos[2][0] = 0.5;
-        desconocidos[2][1] = 0.5;
+        assert(KNN.clasificar(desconocidos, clases, desconocidos[0], 0) == 0);
+        assert(KNN.clasificar(desconocidos, clases, desconocidos[1], 0) == 1);
 
-        for (double[] ds : desconocidos) {
-            System.out.println("\n El clase mas cercana a la corrdenada es : "
-                    + KNN.clasificar(conocidos.getPointsArray(), conocidos.getLabelsArray(), ds, 5));
-        }
+        // for (double[] ds : desconocidos) {
+        //     System.out.println("\n El clase mas cercana a la corrdenada es : "
+        //             + KNN.clasificar(Datos, clases, ds, 1));
+        // }
+        
+        // for (int i = 0; i < desconocidos.length; i++) {
+        // desconocidos[i] = (double) (RandomSeq());
+        // }
+
+        
 
         // System.out.println("los datos en el vector conocidos son: ");
 
